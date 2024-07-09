61e29053f33ad84922e5d365ec8458cafcf6452e
Jake Lee <freejak5520@gmail.com>
2024-07-09 09:21:24 +0900

    Update main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-dvw h-dvh py-8 px-12">
+    <main className="w-dvw h-dvh py-8 px-12 flex flex-col gap-4">
       <Link className="text-xl" href={"/select"}>Select</Link>
       <Link className="text-xl" href={"/horizontal-scroll"}>Horizontal Scroll</Link>
     </main>
