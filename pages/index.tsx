26283ede0194398c02e6bf57a571564f3eded330
Jake Lee <freejak5520@gmail.com>
2024-07-11 12:42:52 +0900

    무한 스크롤 예제 페이지 링크 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="w-dvw h-dvh py-8 px-12 flex flex-col gap-4">
       <Link className="text-xl" href={"/select"}>Select</Link>
       <Link className="text-xl" href={"/horizontal-scroll"}>Horizontal Scroll</Link>
+      <Link className="text-xl" href={"/infinite-scroll"}>Infinite Scroll</Link>
     </main>
   )
 }
